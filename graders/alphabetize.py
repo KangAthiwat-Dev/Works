da36169bcf59c498d23e57aac6226318b740f7c9
KangAthiwat-Dev <kang.athiwat.dev@gmail.com>
2025-11-22 07:46:36 +0700

    Delete graders directory

--- graders/alphabetize.py
+++
@@ -1,3 +0,0 @@
-str_input = input().split(',')
-result = sorted(str_input)
-print(",".join(result))
--- graders/exponent_find_center.py
+++
@@ -1,9 +0,0 @@
-base, power, mid = map(int, input().split())
-result = base ** power
-print(result)
-s = str(result)
-n = len(s)
-start = (n - mid) // 2
-end = start + mid
-middle = s[start:end]
-print(middle)
